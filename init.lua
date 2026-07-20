a7cd2021f3b15b48708771b6eb430ac9b5b3f2ab
metsker <lev.shchinoff@gmail.com>
2026-07-20 23:39:16 +0200

    add transparency

--- init.lua
+++ init.lua
@@ -1,4 +1,13 @@
-vim.pack.add({ 'https://github.com/RRethy/base16-nvim' })
+vim.pack.add({ "https://github.com/RRethy/base16-nvim" })
 
-local ok, matugen = pcall(require, 'matugen')
-if ok then matugen.setup() end
+local ok, matugen = pcall(require, "matugen")
+if ok then
+	matugen.setup()
+end
+
+-- transparent bg
+for _, g in ipairs({ "Normal", "NormalNC", "SignColumn", "LineNr", "EndOfBuffer", "FoldColumn" }) do
+	local hl = vim.api.nvim_get_hl(0, { name = g })
+	hl.bg, hl.ctermbg = nil, nil
+	vim.api.nvim_set_hl(0, g, hl)
+end
